47e6baed5afc2113ecfb25ad5d028bf1a1b1b1a0
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-03-18 11:14:36 +0200

    add the #bubble_sort_by method with solution

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,31 @@
 def bubble_sort(arr)
-   last_index = arr.length
-
-   loop do
+  last_index = arr.length
+  loop do
     sorted = false
     (last_index -1).times do
-        |i|
-        if arr[i] > arr[i + 1] 
-            arr [i], arr [i + 1] =  arr [i + 1], arr [i]
-            sorted = true
-        end
+      |i|
+      if arr[i] > arr[i + 1] 
+          arr [i], arr [i + 1] =  arr [i + 1], arr [i]
+          sorted = true
+      end
     end
-   
-break if sorted == false
-   end
-arr
+    break if sorted == false
+  end
+  arr
 end
 
-a = [2,4,4,6,8,6,9,6,25,1,34,86,34]
-
-p bubble_sort(a)
-
-
+def bubble_sort_by(arr)
+  last_index = arr.length
+  loop do
+    sorted = false
+    (last_index -1).times do
+      |i|
+      if (yield(arr[i], arr[i + 1]) > 0)
+        arr [i], arr [i + 1] =  arr [i + 1], arr [i]
+          sorted = true
+        end
+      end
+    break if sorted == false
+  end
+  arr
+end
